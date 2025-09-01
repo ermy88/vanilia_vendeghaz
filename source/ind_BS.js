1607556d7340b6e93b3860048500bf7d4229f389
ermy88 <ermy.do@gmail.com>
2025-09-01 15:50:59 +0200

    prices.html

--- source/ind_BS.js
+++ source/ind_BS.js
@@ -5,14 +5,22 @@ window.onscroll = function navTop0() {
 
     if (document.documentElement.scrollTop > 5) {
         document.getElementsByTagName('nav')[0].classList = 'header-sticky navBG navbar navbar-expand-lg col-12';
-        
+
     } else if (document.documentElement.scrollTop < 5) {
         document.getElementsByTagName('nav')[0].classList = 'navBG navbar navbar-expand-lg col-12';
     }
 };
 
-// Kezdőlap tartalma
+// Collapsed navbar esetén a nav button-re kattintva megváltozik a nav háttere
 
-document.getElementsByTagName('main')[0].style.top = 0;
 
-console.log();
+    document.getElementsByTagName('nav')[0].addEventListener('click', changeColor);
+
+    function changeColor(){
+        if(document.getElementsByTagName('nav')[0].classList == 'navBG navbar navbar-expand-lg col-12'){
+            document.getElementsByTagName('nav')[0].classList = 'header-sticky navBG navbar navbar-expand-lg col-12';
+        }else{
+            document.getElementsByTagName('nav')[0].classList = 'header-sticky navbar navbar-expand-lg col-12';
+            }
+    }
+
