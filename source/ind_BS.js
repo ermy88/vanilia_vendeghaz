82ef3df853ad4db696a48c4832bcbdec5d8e35b4
ermy88 <ermy.do@gmail.com>
2025-09-03 13:05:34 +0200

    mobil screen optimization

--- source/ind_BS.js
+++ source/ind_BS.js
@@ -1,26 +1,37 @@
 
-//Navbar position -> top 0, background change
+//Navbar position -> top 0, background change if scrolled
 
 window.onscroll = function navTop0() {
 
-    if (document.documentElement.scrollTop > 5) {
+    if (document.documentElement.scrollTop > 1) {
         document.getElementsByTagName('nav')[0].classList = 'header-sticky navBG navbar navbar-expand-lg col-12';
+        document.getElementsByTagName('nav')[0].style.backgroundColor = '#cabfb3';
+        document.getElementsByTagName('nav')[0].style.top = '0vh';
+        document.getElementsByTagName('nav')[0].style.transition = '250ms';
+    } else {
+        document.getElementsByTagName('nav')[0].style.backgroundColor = '#cabfb369';
+        document.getElementsByTagName('nav')[0].style.top = '1vh';
+        document.getElementsByTagName('nav')[0].style.borderRadius = '0rem';
 
-    } else if (document.documentElement.scrollTop < 5) {
-        document.getElementsByTagName('nav')[0].classList = 'navBG navbar navbar-expand-lg col-12';
     }
 };
 
-// Collapsed navbar esetén a nav button-re kattintva megváltozik a nav háttere
+// Mobilnézet
 
 
-    document.getElementsByTagName('nav')[0].addEventListener('click', changeColor);
+document.getElementsByClassName('navbar')[0].addEventListener('mouseenter', () => {
+    document.getElementsByTagName('nav')[0].style.transition = '1000ms';
+    document.getElementsByTagName('nav')[0].style.backgroundColor = '#cabfb3';
+    document.getElementsByClassName('navbar-toggler-icon')[0].style.borderRadius = '5px';
+    document.getElementsByClassName('navbar-toggler-icon')[0].style.transition = '300ms';
+});
 
-    function changeColor(){
-        if(document.getElementsByTagName('nav')[0].classList == 'navBG navbar navbar-expand-lg col-12'){
-            document.getElementsByTagName('nav')[0].classList = 'header-sticky navBG navbar navbar-expand-lg col-12';
-        }else{
-            document.getElementsByTagName('nav')[0].classList = 'header-sticky navbar navbar-expand-lg col-12';
-            }
-    }
+document.getElementsByClassName('navbar')[0].addEventListener('mouseleave', () => {
+    document.getElementsByTagName('nav')[0].style.transition = '1000ms';
+    document.getElementsByTagName('nav')[0].style.backgroundColor = '#cabfb369';
+    document.getElementsByClassName('navbar-toggler-icon')[0].style.border = '2px #644c3a solid';
+    document.getElementsByClassName('navbar-toggler-icon')[0].style.borderRadius = ' 50% / 20%';
+    document.getElementsByClassName('navbar-toggler-icon')[0].style.transition = '300ms';
+});
+
 
